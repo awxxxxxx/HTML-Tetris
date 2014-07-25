8b9d964453eb5dc7a5057b890a5dd61fe21ba6c8
WaterBear <13192872690@163.com>
2014-07-25 16:58:49 +0800

    some files

--- js/diamonds.js
+++ js/diamonds.js
@@ -5,14 +5,17 @@
  *@author:Waterbear
  * */
 
-/***方块图片的路径***/
+/***方块图片的函数,返回一个图片对象***/
 
-var Path = function(n) {
+var image = function() {
     this.s = new Array(12);
+    this.icon = new Array(12);
     for(var i = 0; i < 12; i++){
-        s[i] = '../image/game/' + (i+1) + '.png';
+        s[i] = './image/game/' + (i+1) + '.png';
+        icon[i] = new Image();
+        icon[i].src = s[i];
     }
-    return s[n];
+    return icon;
 
 };
 
@@ -25,8 +28,8 @@ var diaPoint = function(n) {
     }
     this.Point = new Array(7);
     this.Point[0] = [new point(4,0),new point(5,0),new point(6,0),new point(7,0)];
-    this.Point[1] = [new point(4,0),new point(5,1),new point(6,0),new point(5,1)];
-    this.Point[2] = [new point(4,0),new point(5,1),new point(6,0),new point(4,1)];
+    this.Point[1] = [new point(4,0),new point(5,0),new point(6,0),new point(5,1)];
+    this.Point[2] = [new point(4,0),new point(5,0),new point(6,0),new point(4,1)];
     this.Point[3] = [new point(4,1),new point(5,0),new point(5,1),new point(6,0)];
     this.Point[4] = [new point(4,0),new point(5,0),new point(4,1),new point(5,1)];
     this.Point[5] = [new point(4,0),new point(5,0),new point(6,0),new point(6,1)];
@@ -35,19 +38,6 @@ var diaPoint = function(n) {
     return this.Point[n];
 
 };
-
-/******存储图片********/
-
-var Image = function(n) {
-    image = new image
-
-
-};
-
-
-
-
-alert(diaPoint(6)[3].y);
 
 
 
--- js/draw.js
+++ js/draw.js
@@ -5,17 +5,35 @@
  * @author:WaterBear
  * */
 
-/***全局变量****/
+/***画布的全局变量****/
 
 var Global = {
     cvsGame:null,
-    cxtGame:null;
+    cxtGame:null
 
 };
 
 Global.cvsGame = document.getElementById("GameFrame");
 Global.cxtGame = Global.cvsGame.getContext('2d');
 
+/****图像的全局变量*****/
+
+var ImageList = image();
+
+
+/****绘制方块的函数******/
+
+var drawAct = function(context,point,image,n) {
+   for(var i = 0; i < point.length; ++i){
+       context.drawImage(image[n],point[i].x * 32,point[i].y * 32);
+
+   }
+};
+
+$('#start').click(function() {
+   drawAct(Global.cxtGame,diaPoint(4),ImageList,2);
+
+});
 
 
 
